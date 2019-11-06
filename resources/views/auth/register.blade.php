fc224dba882768e9a93036c1442ce77a33094b50
= <=>
2019-11-06 12:16:02 +0500

    Styling login and register Forms

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,68 @@
 @extends('layouts.app')
 
 @section('content')
+
+<div class="columns">
+    <div class="column is-one-third is-offset-one-third m-t-100">
+        <div class="card">
+            <div class="card-content">
+                <h1 class="title">Join the Community</h1>
+                <form action="{{route('register')}}" method="POST" role="form"> {{csrf_field()}}
+                    <div class="field">
+                        <label for="name" class="label">Name</label>
+                        <p class="control">
+                            <input class="input {{ $errors->has('name') ? ' has-danger' : '' }}" type="text" name="name" id="name" value="{{old('name')}}" required>
+                        </p>
+                        @if ($errors->has('name'))
+                            <p class="help is-danger">{{ $errors->first('name') }}</p>
+                        @endif
+                    </div>
+                    <div class="field">
+                        <label for="email" class="label">Email Address</label>
+                        <p class="control">
+                            <input class="input {{ $errors->has('email') ? ' has-danger' : '' }}" type="text" name="email" id="email" value="{{old('email')}}" required>
+                        </p>
+                        @if ($errors->has('email'))
+                            <p class="help is-danger">{{ $errors->first('email') }}</p>
+                        @endif
+                    </div>
+                    <div class="columns">
+                        <div class="column">
+                            <div class="field">
+                                <label for="password" class="label">Password</label>
+                                <p class="control">
+                                    <input class="input {{ $errors->has('password') ? ' has-error' : '' }}" type="password" name="password"
+                                        id="password" required>
+                                </p>
+                                @if ($errors->has('password'))
+                                <p class="help is-danger">{{ $errors->first('password') }}</p>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="column">
+                            <div class="field">
+                                <label for="password_confirmation" class="label">Confirm Password</label>
+                                <p class="control">
+                                    <input class="input {{ $errors->has('password_confirmation') ? ' has-error' : '' }}"
+                                        type="password_confirmation" name="password_confirmation" id="password_confirmation" required>
+                                </p>
+                                @if ($errors->has('password_confirmation'))
+                                <p class="help is-danger">{{ $errors->first('password_confirmation') }}</p>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <button class="button is-primary is-outlined is-fullwidth m-t-30">Register</button>
+                </form>
+            </div>
+            
+        </div>
+        <h5 class="has-text-centered m-t-20"><a href="{{route('login')}}" class="is-muted">Already have an Account?</a></h5>
+    </div>
+</div>
+
+<!--
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
@@ -73,5 +135,5 @@
             </div>
         </div>
     </div>
-</div>
+</div> -->
 @endsection
