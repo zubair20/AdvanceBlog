b2998f04fe977d433c2094a0ae86a6b1d53a0545
= <=>
2019-11-07 14:16:50 +0500

    fixed navbar style bugs

--- config/easynav.php
+++ config/easynav.php
@@ -17,6 +17,6 @@ return [
     | the class defined here will be used if no other custom class is provided.
     |
     */
-    'default_class' => 'is_active',
+    'default_class' => 'is-active',
 
 ];
