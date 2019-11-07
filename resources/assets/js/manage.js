b40afce1f38b4ff3b12e771b5cc2bb65c0ae9e89
= <=>
2019-11-07 13:53:09 +0500

    made sidebar responsive for mobiles

--- resources/assets/js/manage.js
+++ resources/assets/js/manage.js
@@ -1,27 +1,33 @@
 const accordions = document.getElementsByClassName('has-submenu')
+const adminSlideButton = document.getElementById('admin-slideout-button')
+
+function setSubmenuStyles(submenu, maxHeight, margins) {
+  submenu.style.maxHeight = maxHeight
+  submenu.style.marginTop = margins
+  submenu.style.marginBottom = margins
+}
+
+adminSlideButton.onclick = function () {
+  this.classList.toggle('is-active');
+  document.getElementById('admin-side-menu').classList.toggle('is-active');
+}
 
 for (var i = 0; i < accordions.length; i++) {
   if (accordions[i].classList.contains('is-active')) {
-    const submenu = accordions[i].nextElementSibling;
-    submenu.style.maxHeight = submenu.scrollHeight + "px";
-    submenu.style.marginTop = "0.75em"
-    submenu.style.marginBottom = "0.75em"
+    const submenu = accordions[i].nextElementSibling
+    setSubmenuStyles(submenu, submenu.scrollHeight + "px", "0.75em")
   }
 
   accordions[i].onclick = function () {
-    this.classList.toggle('is-active');
+    this.classList.toggle('is-active')
 
-    const submenu = this.nextElementSibling;
+    const submenu = this.nextElementSibling
     if (submenu.style.maxHeight) {
       // menu is open, we need to close it now
-      submenu.style.maxHeight = null
-      submenu.style.marginTop = null
-      submenu.style.marginBottom = null
+      setSubmenuStyles(submenu, null, null)
     } else {
       // meny is close, so we need to open it
-      submenu.style.maxHeight = submenu.scrollHeight + "px"
-      submenu.style.marginTop = "0.75em"
-      submenu.style.marginBottom = "0.75em"
+      setSubmenuStyles(submenu, submenu.scrollHeight + "px", "0.75em")
     }
   }
 }
